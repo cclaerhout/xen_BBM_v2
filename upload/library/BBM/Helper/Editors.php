35ff2c51fc53a1d7e575ac90ea6718b7f7048f8c
cclaerhout <cedric.claerhout@free.fr>
2013-12-14 14:20:49 +0800

    Editors checker fix for XenForo 1.1.x

--- upload/library/BBM/Helper/Editors.php
+++ upload/library/BBM/Helper/Editors.php
@@ -7,8 +7,9 @@ class BBM_Helper_Editors
 		$redactorSupport = (XenForo_Application::get('options')->get('currentVersionId') >= 1020031);
 		$mceSupport = !$redactorSupport;
 		
-		if (method_exists('Sedo_TinyQuattro_Helper_Quattro', 'isEnabled'))
+		if (method_exists('Sedo_TinyQuattro_Helper_Quattro', 'isEnabled') && $redactorSupport)
 		{
+			//$redactorSupport added to conditional to match only XenForo > 1.2
 			$activeAddons =  XenForo_Application::get('addOns');
 			$mceSupport = (!empty($activeAddons['sedo_tinymce_quattro'])) ? true : false;
 		}
