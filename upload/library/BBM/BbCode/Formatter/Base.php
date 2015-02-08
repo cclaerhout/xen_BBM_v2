4dbf9071842b0cebe7a25bd54a715d0d3dcf6af2
Xon <tazipper1@hotmail.com>
2015-02-09 07:54:59 +0800

    Create non-cached tag map on thread-preview.

--- upload/library/BBM/BbCode/Formatter/Base.php
+++ upload/library/BBM/BbCode/Formatter/Base.php
@@ -1715,7 +1715,33 @@ class BBM_BbCode_Formatter_Base extends XFCP_BBM_BbCode_Formatter_Base
 
 				$this->_createBbCodesMap($this->_postsDatas, 'post');
 			}
+			/**
+			 *  Preview new post in thread or new thread
+			 **/
+			else if( (!isset($params['posts']) && isset($params['thread']) || isset($params['forum']))  && isset($params['message']) 
+				&& $this->_disableTagsMap == false && !isset($params['bbm_config']))
+            {
+                $visitor = XenForo_Visitor::getInstance()->ToArray();
+                
+				$this->_bbmMessageKey = 'message';
 
+                if (isset($params['thread']))
+                    $this->_threadParams = $params['thread'];
+                else
+                    $this->_threadParams = array( 'node_id' => $params['forum']['node_id']);
+				$this->_postsDatas = array ( 0 => array
+                        (
+                            'post_date' => XenForo_Application::$time, 
+                            'user_id' => $visitor['user_id'], 
+                            'post_id' => 0, 
+                            'user_group_id' => $visitor['user_group_id'], 
+                            'secondary_group_ids' => $visitor['secondary_group_ids'],
+                            'message' => $params['message']
+                        )
+                );
+
+				$this->_createBbCodesMap($this->_postsDatas, Null);
+            }
 			/**
 			 *  For conversations: check conversation & messages
 			 *  It's not perfect, but let's use the same functions than thread & posts
