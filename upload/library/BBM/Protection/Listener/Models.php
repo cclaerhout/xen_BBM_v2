406b83c3e625c795085ef275552699eb83044716
Xon <tazipper1@hotmail.com>
2016-07-24 14:24:47 +0800

    Bugfix nested bbcode parser permissions

--- upload/library/BBM/Protection/Listener/Models.php
+++ upload/library/BBM/Protection/Listener/Models.php
@@ -4,19 +4,17 @@ class BBM_Protection_Listener_Models
 {
 	public static function protectModels($class, array &$extend)
 	{
-		if ($class == 'XenForo_Model_NewsFeed')
+        switch($class)
 		{
-			$extend[] = 'BBM_Protection_Model_NewsFeed';
-		}
-		
-		if ($class == 'XenForo_Model_Search')
-	      	{
-			$extend[] = 'BBM_Protection_Model_Search';	      	
-	      	}
-
-	        if ($class == 'XenForo_Model_ThreadWatch')
-      		{
-			$extend[] = 'BBM_Protection_Model_ThreadWatch';
-	      	} 
+            case 'XenForo_Model_NewsFeed':
+                $extend[] = 'BBM_Protection_Model_NewsFeed';
+                break;
+            case 'XenForo_Model_Search':
+                $extend[] = 'BBM_Protection_Model_Search';
+                break;
+            case 'XenForo_Model_ThreadWatch':
+                $extend[] = 'BBM_Protection_Model_ThreadWatch';
+                break;
+        }
 	}
 }
