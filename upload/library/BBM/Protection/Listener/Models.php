f7b09eac57f26bd7eefe0a210eda1e3da853012e
cclaerhout <cedric.claerhout@free.fr>
2016-08-11 14:05:31 +0200

    Update Models.php

--- upload/library/BBM/Protection/Listener/Models.php
+++ upload/library/BBM/Protection/Listener/Models.php
@@ -4,17 +4,17 @@ class BBM_Protection_Listener_Models
 {
 	public static function protectModels($class, array &$extend)
 	{
-        switch($class)
+        	switch($class)
 		{
-            case 'XenForo_Model_NewsFeed':
-                $extend[] = 'BBM_Protection_Model_NewsFeed';
-                break;
-            case 'XenForo_Model_Search':
-                $extend[] = 'BBM_Protection_Model_Search';
-                break;
-            case 'XenForo_Model_ThreadWatch':
-                $extend[] = 'BBM_Protection_Model_ThreadWatch';
-                break;
-        }
+        		case 'XenForo_Model_NewsFeed':
+        			$extend[] = 'BBM_Protection_Model_NewsFeed';
+                	break;
+            		case 'XenForo_Model_Search':
+                		$extend[] = 'BBM_Protection_Model_Search';
+                	break;
+        		case 'XenForo_Model_ThreadWatch':
+                		$extend[] = 'BBM_Protection_Model_ThreadWatch';
+                	break;
+        	}
 	}
 }
